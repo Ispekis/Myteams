fe1b0977b40e7a734b67ebf13202736d60617012
zvillon <zhg.villon@gmail.com>
2023-04-20 00:36:53 +0200

    feat(unsubscribe)Unsubscribe feature

---
+++ src/client/src/receiver/unsubscribe.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** B-NWP-400-PAR-4-1-myteams-jeras.bertine
+** File description:
+** unsubscribe
+*/
+
+#include "client.h"
+
+int recv_unsubscribe(client_t *client, server_packet recv_data)
+{
+    char team_uuid[37];
+    char user_uuid[37];
+
+    uuid_unparse(recv_data.team_uuid, team_uuid);
+    uuid_unparse(recv_data.user_uuid, user_uuid);
+    client_print_unsubscribed(team_uuid, user_uuid);
+    return 0;
+}
